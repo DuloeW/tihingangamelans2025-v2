e92cbf1e5197ce5206c1ae47618858b6e102e40d
DuloeW <duloekeren@gmail.com>
2025-11-09 17:53:12 +0800

    edit footer belum sepenuhnya

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,50 +1,48 @@
-<footer class="relative h-fit min-h-80 w-auto flex justify-between bg-primary px-20 py-10 text-white font-markazi">
-
-    <p class="absolute bottom-1 left-1/2 transform -translate-x-1/2 text-white font-markazi text-sm">
-        &copy; 2025 Tihingan Gamelans. All rights reserved.
-    </p>
-
-    <div class="flex-1 flex flex-col">
+<footer class="bg-primary text-white font-markazi px-10 md:px-20 py-10">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-10 items-start">
+        <!-- Kolom 1: Navigasi -->
         <div>
-            <ul class="flex justify-around gap-10 w-auto">
-                <li>
-                    <a href="">Home</a>
-                </li>
-                <li>
-                    <a href="">Gallery</a>
-                </li>
-                <li>
-                    <a href="">Store</a>
-                </li>
+            <h3 class="text-xl font-semibold mb-4">Navigasi</h3>
+            <ul class="space-y-2">
+                <li><a href="#" class="hover:text-yellow-400 transition">Home</a></li>
+                <li><a href="#" class="hover:text-yellow-400 transition">Gallery</a></li>
+                <li><a href="#" class="hover:text-yellow-400 transition">Store</a></li>
             </ul>
         </div>
 
-        <div class="w-1/2">
-            <p>Gabung bersama kami dan jelajahi jutaan gamelan dari pengerajin terpercaya!</p>
+        <!-- Kolom 2: Deskripsi -->
+        <div class="text-center md:text-left">
+            <h3 class="text-xl font-semibold mb-4">Tentang Kami</h3>
+            <p class="text-sm leading-relaxed tracking-wider">
+                Gabung bersama kami dan jelajahi jutaan gamelan dari pengerajin terpercaya!  
+                Setiap nada, setiap ukiran, adalah warisan budaya yang hidup.
+            </p>
+
+            <!-- Ikon Sosial -->
+            <div class="flex justify-center md:justify-start gap-5 mt-5">
+                <a href="#" class="hover:scale-110 transition">
+                    <img src="{{ asset('images/instagram-logo.svg') }}" alt="Instagram Logo" class="h-6 w-6 ">
+                </a>
+                <a href="#" class="hover:scale-110 transition">
+                    <img src="{{ asset('images/youtube-logo.svg') }}" alt="YouTube Logo" class="h-6 w-6">
+                </a>
+                <a href="#" class="hover:scale-110 transition">
+                    <img src="{{ asset('images/telephone-logo.svg') }}" alt="Telephone Logo" class="h-6 w-6">
+                </a>
+            </div>
+        </div>
+
+        <!-- Kolom 3: Map -->
+        <div class="flex justify-center md:justify-end">
+            <iframe class="rounded-lg shadow-lg"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31565.679682113587!2d115.3631689345516!3d-8.527514004691785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd21132054a3c09%3A0x5030bfbca8320b0!2sTihingan%2C%20Kec.%20Banjarangkan%2C%20Kabupaten%20Klungkung%2C%20Bali!5e0!3m2!1sid!2sid!4v1762603691803!5m2!1sid!2sid"
+                width="350" height="200" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 
-    <div class="flex-1 flex flex-col">
-        <div>
-            <ul class="flex justify-around gap-10 w-auto">
-                <li>
-                    <a href="">
-                        <img src="{{ asset('images/instagram-logo.svg') }}" alt="Instagram Logo" class="h-6 w-6">
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="{{ asset('images/youtube-logo.svg') }}" alt="YouTube Logo" class="h-6 w-6">
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <img src="{{ asset('images/telephone-logo.svg') }}" alt="Telephone Logo" class="h-6 w-6">
-                    </a>
-                </li>
-            </ul>
-        </div>
-        {{-- <img src="{{ asset('images/logo_vektor_02.svg') }}" alt="" class="h-28 min-h-20 w-auto p-5 rounded-lg bg-white"> --}}
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31565.679682113587!2d115.3631689345516!3d-8.527514004691785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd21132054a3c09%3A0x5030bfbca8320b0!2sTihingan%2C%20Kec.%20Banjarangkan%2C%20Kabupaten%20Klungkung%2C%20Bali!5e0!3m2!1sid!2sid!4v1762603691803!5m2!1sid!2sid" width="400" height="250" class="rounded-lg border-0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <!-- Garis pemisah -->
+    <div class="border-t border-gray-500 mt-10 pt-4 text-center text-sm">
+        &copy; 2025 <span class="text-yellow-400">Tihingan Gamelans</span>. All rights reserved.
     </div>
 </footer>
