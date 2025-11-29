ac7d3b3ac8d8e703adedeef1d9fa7d6820150bda
DuloeW <duloekeren@gmail.com>
2025-11-29 21:58:42 +0800

    Refactor code structure for improved readability and maintainability

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-class Admin extends Model
+class Admin extends Authenticatable implements FilamentUser, HasName
 {
-    //
+    use HasUuids;
+    
+    protected $table = 'admin';
+    protected $primaryKey = 'admin_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama', 
+        'email', 
+        'password',
+        'user_name',    
+        'no_telephone'
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Return true agar semua data di tabel admin bisa login ke panel
+        return true; 
+    }
+    
+    public function getFilamentName(): string
+    {
+        // Beritahu Filament bahwa nama user ada di kolom 'nama'
+        return $this->nama;
+    }
 }
