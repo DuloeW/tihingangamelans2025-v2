da2188e4fc27fc146954f245783cfdeca322d8a3
DuloeW <duloekeren@gmail.com>
2025-11-30 00:43:32 +0800

    store [70%]

--- resources/views/Store.blade.php
+++ resources/views/Store.blade.php
@@ -7,7 +7,7 @@
                     ensemble. It is a large, vertically mounted gong that produces a deep, resonant sound when struck with a padded mallet. The Gong Sigar is often used to mark the beginning and end of musical pieces in gamelan music.',
             'profile' => 'images/bende1.png',
             'dataAos' => 'fade-up',
-            'dataAosDuration' => '1500'
+            'dataAosDuration' => '1500',
         ],
         [
             'nama' => 'Kendang',
@@ -16,7 +16,7 @@
                     ensemble. It is a double-headed drum that is played with the hands and produces a variety of rhythms and sounds. The Kendang is often used to lead the ensemble and provide rhythmic accompaniment to other instruments.',
             'profile' => 'images/bende1.png',
             'dataAos' => 'fade-up',
-            'dataAosDuration' => '1500'
+            'dataAosDuration' => '1500',
         ],
         [
             'nama' => 'Kendang',
@@ -25,7 +25,7 @@
                     ensemble. It is a double-headed drum that is played with the hands and produces a variety of rhythms and sounds. The Kendang is often used to lead the ensemble and provide rhythmic accompaniment to other instruments.',
             'profile' => 'images/bende1.png',
             'dataAos' => 'fade-up',
-            'dataAosDuration' => '1500'
+            'dataAosDuration' => '1500',
         ],
         [
             'nama' => 'Kendang',
@@ -34,9 +34,9 @@
                     ensemble. It is a double-headed drum that is played with the hands and produces a variety of rhythms and sounds. The Kendang is often used to lead the ensemble and provide rhythmic accompaniment to other instruments.',
             'profile' => 'images/bende1.png',
             'dataAos' => 'fade-up',
-            'dataAosDuration' => '1500'
+            'dataAosDuration' => '1500',
         ],
-    ]
+    ];
 @endphp
 
 <x-global-layout>
@@ -45,12 +45,28 @@
     </x-slot:title>
 
     <section id="store" class="min-h-[480px] px-20 pb-20 font-markazi">
-        <div class="container mx-auto px-4">
+        <div class="container mx-auto">
             <h2 class="text-center text-8xl py-10">Store</h2>
-            <div>
-                //TODO pencarian store
+
+            <div class="w-full grid place-items-center">
+                <div class="max-w-3xl w-full flex justify-center items-stretch gap-3">
+                    <div
+                        class="flex-1 flex items-center bg-white rounded-lg shadow-[0_4px_15px_-3px_rgba(0,0,0,0.07)] px-4 py-3.5">
+                        <x-search-icon />
+    
+                        <input type="text" placeholder="Cari Toko Kerajinan"
+                            class="w-full ml-4 text-lg bg-transparent outline-none border-none text-gray-800 placeholder-gray-800 font-input tracking-wide focus:border-none" />
+                    </div>
+    
+                    <button
+                        class="bg-white rounded-lg shadow-[0_4px_15px_-3px_rgba(0,0,0,0.07)] px-4 flex items-center justify-center hover:bg-gray-50 transition-colors">
+                        <x-filter-search-icon />
+                    </button>
+                </div>
+
             </div>
-            {{-- <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+
+            <div class="mt-24    grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 @foreach ($stores as $store)
                     <x-box-store 
                         :nama="$store['nama']" 
@@ -61,7 +77,7 @@
                         :dataAosDuration="$store['dataAosDuration']" 
                     />
                 @endforeach
-            </div> --}}
+            </div>
         </div>
     </section>
 </x-global-layout>
